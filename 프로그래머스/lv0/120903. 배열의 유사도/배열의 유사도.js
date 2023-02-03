acd6f017576224c5020cc45026a9b7ae41352ca8
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-02-03 16:17:37 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,11 @@
+function solution(s1, s2) {
+    let answer = 0;
+   
+    for(let one of s1){
+        for(let two of s2){
+            if(one === two) answer++;
+        }
+    }
+    
+    return answer;
+}
