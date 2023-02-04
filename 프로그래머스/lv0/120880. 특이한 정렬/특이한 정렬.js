623f960d483f0e28c4e70bd666d8947f7f1b3b23
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-02-05 01:33:16 +0900

    [level 0] Title: 특이한 정렬, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,14 @@
+function solution(numlist, n) {
+    let answer = numlist.slice();
+    
+    answer.sort((a,b) => {
+        let diffA = (n - a) < 0 ? (n-a) * (-1) : (n-a);
+        let diffB = (n - b) < 0 ? (n-b) * (-1) : (n-b);
+        
+        if(diffA === diffB) return b - a;
+        else return diffA - diffB;
+    })
+    
+    
+    return answer;
+}
