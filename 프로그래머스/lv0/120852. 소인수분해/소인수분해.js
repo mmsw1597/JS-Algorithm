0c8949e4eefef8973410f418601078e673647e65
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-02-07 01:57:23 +0900

    [level 0] Title: 소인수분해, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let answer = [];
+    let tmp = n;
+    
+    for(let i=2; i<=Math.floor(n/2); i++){
+        if(tmp%i === 0){
+            answer.push(i);
+            tmp /= i;
+            i--;
+        } 
+    }
+    
+    if(!answer.length) answer.push(n);
+    answer = [...new Set(answer)]
+    
+    return answer;
+}
