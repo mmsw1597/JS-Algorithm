42d6c948c74ada92e6e4f1bec6fc1cd9edab456d
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-02-09 11:46:49 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let answer = [0, 0];
+    
+    for(let x of num_list){
+        if(x % 2 === 0) answer[0]++;
+        else answer[1]++;
+    }
+    
+    return answer;
+}
