ed060d8e6c8e302aa8a2d034eba024cddb6c5a46
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-02-11 22:52:44 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.05 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,30 +1,16 @@
 function solution(number, limit, power) {
     let answer = 0;
-    let ch = Array.from({length: number+1}, ()=>0);
     
-    function cal(n){
-        let map = new Map();
-        let fix = n;
-        for(let i = 2; i<= n; i++){
-            while((n % i) === 0){
-                if(map.has(i)) map.set(i, map.get(i) + 1);
-                else map.set(i, 2);
-                n/=i;
-            }
-        }     
-        let arr = [...map.values()];
-        let sum = arr.reduce((acc, v)=>{
-            return acc * (v);
-        }, 1)
+    for(let n = 1; n<=number; n++){
+        let count = 0;
         
-        return sum;
+        for(let j = 1; j*j <= n; j++){
+            if(j * j === n) count++;
+            else if((n % j) === 0) count += 2; 
+        }
+        
+        if(count > limit) answer += power;
+        else answer += count;
     }
-    
-    for(let i = 1; i<=number; i++){
-        let x = cal(i);
-        if(x > limit) answer += power;
-        else answer += x;
-    }
-    
     return answer;
 }
