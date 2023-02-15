93e207141e71d63864324cf09afc31c419ee60a0
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-02-16 02:14:30 +0900

    [level 1] Title: 최소직사각형, Time: 6.65 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,38 +1,12 @@
 function solution(sizes) {
-    let answer = Number.MAX_SAFE_INTEGER;
     let W = 0;
     let H = 0;
     
-    for(let x of sizes){
-        let width = x[0];
-        let height = x[1];
-             
-        let case1 = Math.max(W, width) * Math.max(H, height);
-        let case2 = Math.max(W, height) * Math.max(H, width);
-            
-        if(case1 >= case2){
-            W = Math.max(W, height);
-            H = Math.max(H, width);
-        }else{
-            W = Math.max(W, width);
-            H = Math.max(H, height);
-        }
-    }
-    
-    let width = sizes[0][0];
-    let height = sizes[0][1];
-    
-    let case1 = Math.max(W, width) * Math.max(H, height);
-        let case2 = Math.max(W, height) * Math.max(H, width);
-            
-        if(case1 >= case2){
-            W = Math.max(W, height);
-            H = Math.max(H, width);
-        }else{
-            W = Math.max(W, width);
-            H = Math.max(H, height);
-        }
-    
+    sizes.forEach((v, i) => {
+        let [w, h] = v.sort((a,b) => b - a);
+        W = Math.max(W, w);
+        H = Math.max(H, h);
+    })
     
     return W*H;
 }
