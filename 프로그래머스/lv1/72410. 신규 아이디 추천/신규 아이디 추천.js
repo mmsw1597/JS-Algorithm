a5418a8ac72d85eb5381ef56b6e80516e371bdb6
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-02-25 05:02:50 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,27 +1,17 @@
 function solution(new_id) {
-    let answer = new_id.split("");
+    let answer = new_id.toLowerCase()
+                .replace(/[^\w-.]/g, "")
+                .replace(/\.+/g, ".")
+                .replace(/^\.|\.$/, "")
+                .replace(/^$/, "a")
+                .slice(0, 15).replace(/\.$/, "");
+                
+    const len = answer.length;
     
-    answer = answer.map((v)=>{
-        return v.toLowerCase();
-    })
-    
-    answer = answer.join("");
-    answer = answer.replace(/[^0-9a-z._-]/gi, "");
-    answer = answer.replace(/[.]+/g, ".");
-    answer = answer.replace(/^\./g, "");
-    answer = answer.replace(/\.$/g, "");
-    if(answer.length === 0) answer = "a";
-    if(answer.length >= 16){
-        answer = answer.substring(0, 15);
-        answer = answer.replace(/\.$/g, "");
+    if(len <= 2){
+        while(answer.length !== 3)
+            answer += answer[len-1];
     }
-    if(answer.length <= 2){
-        while(answer.length !== 3){
-            answer += answer[answer.length - 1];
-        }
-    }
-    
-    console.log(answer);
     
     return answer;
 }
