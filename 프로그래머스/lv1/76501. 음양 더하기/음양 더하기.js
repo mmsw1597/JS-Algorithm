78ca3edce4e6d3202fc84aa74ba9dec1ef3560ed
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-02-25 04:16:47 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,10 @@
+function solution(absolutes, signs) {
+    let answer = 0;
+    
+    absolutes.forEach((v, i)=>{
+        if(signs[i]) answer += v;
+        else answer -= v;
+    })
+    
+    return answer;
+}
