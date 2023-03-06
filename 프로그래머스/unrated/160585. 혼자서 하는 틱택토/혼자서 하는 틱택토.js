5f8bc40725f41fb3b20d792efb49014bffba6787
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-03-06 18:06:19 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.js
@@ -0,0 +1,54 @@
+function solution(board) {
+    let answer = 1;
+    const win = {
+        O : 0,
+        X : 0,
+    }
+    const count = {
+        O : 0,
+        X : 0,
+    }
+    
+    for(let i = 0; i<3; i++){
+        for(let j= 0 ; j<3; j++){
+            if(board[i][j] !== "."){
+                count[board[i][j]]++;
+            }
+        }
+    }
+    
+    for(let i = 0; i<3; i++){
+        if(board[i][0] !== "."){
+            if(board[i][0] === board[i][1] && board[i][0] === board[i][2]){
+                win[board[i][0]]++;
+            }
+        }
+    }
+    
+    for(let i = 0; i<3; i++){
+        if(board[0][i] !== "."){
+            if(board[0][i] === board[1][i] && board[0][i] === board[2][i]){
+                win[board[0][i]]++;
+            }
+        }
+    }
+    
+    if(board[0][0] !== "."){
+        if(board[0][0] === board[1][1] && board[0][0] === board[2][2]){
+            win[board[0][0]]++;
+        }
+    }
+    
+    if(board[0][2] !== "."){
+        if(board[0][2] === board[1][1] && board[0][2] === board[2][0]){
+            win[board[0][2]]++;
+        }
+    }
+    
+    if(count.X > count.O) answer = 0;
+    if(win.X && win.O) answer = 0
+    if(count.O - count.X >= 2) answer = 0;
+    if(win.X && count.X !== count.O) answer = 0;
+    if(win.O && count.O - count.X !== 1) answer = 0;
+    return answer;
+}
