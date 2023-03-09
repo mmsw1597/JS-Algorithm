5d64ccbd125881cdd5652568791dbc7c70dc484d
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-03-09 21:02:36 +0900

    [unrated] Title: 유사 칸토어 비트열, Time: 12.84 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
@@ -0,0 +1,26 @@
+function solution(n, l, r) {
+    let answer = 0;
+    const zero = [];
+    
+    for(let i = 1; i<=n; i++){
+        const startZero = (5 ** (i-1))*2 + 1;
+        const endZero = (5 ** (i-1)) * 3;
+        zero.push([startZero, endZero]);    
+    }
+    console.log(zero);
+    function isZero(num){
+        let tmp = 5;
+        for(let [s, e] of zero){
+            const d = num % tmp;
+            if(d >= s && d <= e)
+                return true;
+            tmp *= 5;
+        }
+        return false;
+    }
+    for(let i = l; i<=r; i++){
+        if(!isZero(i)) answer++;
+    }
+    
+    return answer;
+}
