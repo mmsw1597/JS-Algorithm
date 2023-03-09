0cabde4e779f2ea5eb79ada90bdc96eddd7d2831
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-03-09 21:33:07 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.30 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -0,0 +1,50 @@
+function solution(users, emoticons) {
+    let answer = [-1, -1];
+    const sales = [];
+    
+    function cal(){
+        let money = 0;
+        let join = 0;
+        
+        users.forEach((v, i)=> {
+            const [discount, max] = v;
+            let sum = 0;
+            
+            for(let i = 0; i<emoticons.length; i++){
+                if(discount <= sales[i]){
+                    sum += emoticons[i] * ((100 - sales[i]) / 100);
+                }
+            }
+            if(sum >= max) join++;
+            else money += sum;
+        })
+        
+        return [join, money];
+    }
+    
+    function DFS(idx){
+        if(sales.length === emoticons.length){
+            const [join, money] = cal();
+            if(join > answer[0]) answer = [join, money];
+            else if(join === answer[0]){
+                if(money > answer[1])
+                    answer = [join, money];
+            }
+            return;
+        }    
+        
+        for(let i = idx; i<emoticons.length; i++){
+            for(let sale = 10; sale <= 40; sale += 10){
+                sales.push(sale);
+                DFS(i+1);
+                sales.pop();
+            }
+        }
+    }
+    
+    DFS(0);
+    
+    answer
+    
+    return answer;
+}
