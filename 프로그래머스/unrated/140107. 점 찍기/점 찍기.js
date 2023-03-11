919a5ea2fe566d19ffb2176ecd6bad446843b14d
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-03-12 01:21:58 +0900

    [unrated] Title: 점 찍기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.js
@@ -0,0 +1,12 @@
+function solution(k, d) {
+    let answer = 0;
+    const rad = Math.floor(Math.sqrt(d**2 / k**2));
+    
+    for(let a = 0; a<=rad; a++){
+        const tmp = Math.floor(Math.sqrt((d**2/k**2) - a**2));
+        answer += tmp + 1;
+    }
+    
+    
+    return answer;
+}
