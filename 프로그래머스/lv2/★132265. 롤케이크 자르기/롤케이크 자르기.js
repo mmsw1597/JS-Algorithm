3bf6fd1b602d59cfd6eb1ba303ac19ed6722f211
mmsw1597 <mmsw1597@naver.com>
2023-03-12 18:45:54 +0900

    fix: 중요문제 별표 및 주석처리

---
+++ 프로그래머스/lv2/★132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,29 @@
+function solution(topping) {
+  let answer = 0;
+  let left = {};
+  let right = {};
+  let l = 0;
+  let r = 0;
+
+  for (let x of topping) {
+    right[x] = right[x] + 1 || 1;
+  }
+  r = Object.keys(right).length;
+
+  for (let x of topping) {
+    if (!left[x]) {
+      left[x] = true;
+      l++;
+    }
+    if (right[x] === 1) {
+      r--;
+    } else {
+      right[x]--;
+    }
+    if (l === r) answer++;
+  }
+
+  return answer;
+}
+
+// 해시
