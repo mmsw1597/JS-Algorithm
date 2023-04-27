227e094c74be96a39995dbe402911adb83d189d2
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-04-27 23:31:19 +0900

    [unrated] Title: 공원 산책, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
@@ -0,0 +1,45 @@
+function solution(park, routes) {
+    let answer = [];
+    let startY = 0;
+    let startX = 0;
+    const H = park.length;
+    const W = park[0].length;
+    const direction = {
+        N : [-1, 0],
+        E : [0, 1],
+        S : [1, 0],
+        W : [0, -1]
+    };
+    
+    for(let i = 0; i<park.length; i+=1){
+        for(let j = 0; j<park[i].length; j+=1){
+            if(park[i][j] === "S"){
+                startY = i;
+                startX = j;
+                break;
+            }
+        }
+    }
+    
+    for(let piece of routes){
+        let [dir, dis] = piece.split(" ");
+        let [dy, dx] = direction[dir];
+        let flag = false;
+        let ny = null;
+        let nx = null;
+        
+        for(let i = 1; i<=dis; i+=1){
+            ny = startY + (dy * i);
+            nx = startX + (dx * i);
+            if(ny >= H || ny < 0 || nx >= W || nx < 0 || park[ny][nx] === "X"){
+                flag = true;
+                break;
+            }
+        }
+        if(flag) continue;
+        startY = ny;
+        startX = nx;
+    }
+
+    return [startY, startX];
+}
