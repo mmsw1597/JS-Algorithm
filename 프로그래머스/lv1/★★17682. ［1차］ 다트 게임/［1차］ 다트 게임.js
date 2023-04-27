6ba217682caee79a0086d5dd9957eb618cba6bcb
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-04-28 05:19:26 +0900

    Update ［1차］ 다트 게임.js

--- 프로그래머스/lv1/★★17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/★★17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,43 +1,49 @@
+// 스타상은 해당 점수와 바로 전에 얻은 점수를 2배로
+// 아차상은 해당 점수를 마이너스로
+// 스타상 중첩 가능 아차상과도 중첩 가능
+
+
 function solution(dartResult) {
-  let answer = 0;
-  let score = [];
-  let parse = [];
-  let idx = 0;
-  const mul = {
-    S: 1,
-    D: 2,
-    T: 3,
-  };
-
-  dartResult.split(/[SDT#*]/).forEach((v) => {
-    if (v) {
-      score.push(+v);
+    let answer = 0;
+    const option = [];
+    
+    // 점수만 분리하도록 split 사용. 두 자릿수 정수도 있는 것에 주의
+    const score = (dartResult.split(/[SDT*#]/g)).filter(v => v).map(v => +v);
+  
+    // SDT만 구분하도록 분리
+    const bonus = (dartResult.replace(/[0-9*#]/g, ""));
+    
+    // 옵션이 붙어있는 점수의 인덱스를 따로 저장
+    (dartResult.split(/[SDT]/g)).map((v, i) => {
+        if(!v) return;
+        
+        if(v.includes("#")) option.push(["#", i - 1]);
+        else if(v.includes("*")) option.push(["*", i - 1]);
+    })
+    
+    let idx = 0;
+    const result = score.slice();
+    
+    for(let i = 0; i<score.length; i+=1){
+        const cur = result[i];
+        if(bonus[i] === "D") result[i] = cur * cur;
+        else if(bonus[i] === "T") result[i] = cur * cur * cur;
     }
-  });
-  console.log(score);
-  dartResult.split(/[0-9]/).forEach((v) => {
-    if (v) {
-      parse.push(v);
+    
+    for(let [type, index] of option){
+        if(type === "#") result[index] *= (-1);
+        else if(type === "*"){
+            if(index > 0) result[index - 1] *= 2;
+            result[index] *= 2;
+        }
     }
-  });
-  console.log(parse);
-
-  for (let x of parse) {
-    score[idx] = score[idx] ** mul[x[0]];
-    if (x[1] === "*") {
-      score[idx] *= 2;
-      score[idx - 1] *= 2;
-    } else if (x[1] === "#") {
-      score[idx] *= -1;
-    }
-    console.log(score);
-    idx++;
-  }
-
-  for (let i = 0; i < 3; i++) {
-    answer += score[i];
-  }
-  return answer;
+    
+    return result.reduce((acc, v) => acc + v, 0);
 }
 
 //정규표현식을 잘 다뤄야 빨리 품
+// ============================ 수정 ====================================
+
+// 다시 풀어도 난해하기 그지 없다.
+// split을 정규표현식으로 구분하는 법을 알아야 함
+// 옵션은 맨마지막에 적용하는게 깔끔한거 같음
