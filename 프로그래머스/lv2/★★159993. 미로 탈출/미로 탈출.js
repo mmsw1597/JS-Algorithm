fca2f03e8653e9c9a9861cd3168cbaccb0946779
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-04-29 13:10:18 +0900

    Update 미로 탈출.js

--- 프로그래머스/lv2/★★159993. 미로 탈출/미로 탈출.js
+++ 프로그래머스/lv2/★★159993. 미로 탈출/미로 탈출.js
@@ -63,3 +63,80 @@ function solution(maps) {
 //첫 싸이클에는 큐에 노드를 삽입하기 전에 있던 노드만 고려해야함. 큐에 삽입한 거까지 반복문이 돌지 않도록 주의
 //출발지에서 목적지까지 거리는 곧 노드의 레벨을 의미.
 //BFS문제로 매우 좋은 문제 반드시 다시 풀자
+
+// 수정
+/*
+function solution(maps) {
+    let answer = 0;
+    let startY = 0;
+    let startX = 0;
+    let leverY = 0;
+    let leverX = 0;
+    let endY = 0;
+    let endX = 0;
+    let flag = false;
+    let w = maps[0].length;
+    let h = maps.length;
+    const d = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+    
+    for(let i = 0; i<maps.length; i+=1){
+        for(let j = 0; j<maps[i].length; j+=1){
+            if(maps[i][j] === "S"){
+                startY = i;
+                startX = j;
+            }
+            else if(maps[i][j] === "E"){
+                endY = i;
+                endX = j;
+            }
+            else if(maps[i][j] === "L"){
+                leverY = i;
+                leverX = j;
+            }
+        }
+    }
+    
+    const BFS = (sy, sx, target) => {
+        let queue = [[sy, sx]];
+        let level = 0;
+        const visited = Array.from({length : h}, () => Array(w).fill(false));
+        visited[sy][sx] = true;
+        
+        while(queue.length){
+            const tmp = queue;
+            queue = [];
+
+            for(let [y, x] of tmp){
+                if(maps[y][x] === target)
+                    return level;
+                
+                for(let [dy, dx] of d){
+                    let ny = dy + y;
+                    let nx = dx + x;
+                    
+                    if(ny >= h || ny < 0 || nx >= w || nx < 0 || maps[ny][nx] === "X")
+                        continue;
+                    
+                    if(visited[ny][nx])
+                        continue;
+    
+                    visited[ny][nx] = true;
+                    queue.push([ny, nx]);
+                }
+            }
+            level += 1;
+        }
+        
+        return -1;
+    };
+    
+    const s = BFS(startY, startX, "L");
+    if(s === -1) return -1;
+    const e = BFS(leverY, leverX, "E");
+    if(e === -1) return -1;
+    
+    return s + e;
+}
+*/
+// 첫번째로 풀었던 풀이와는 크게 다르진 않은 것 같음
+// 당시엔 queue 구현한답시고 shift 썼는데 O(1)인줄 알고 썼나봄
