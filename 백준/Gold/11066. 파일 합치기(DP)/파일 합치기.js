8baca77d68004fc157ecde0cef38534beffa0b70
mmsw1597 <mmsw1597@naver.com>
2023-05-06 08:03:54 +0900

    fix: 백준 문제 주석 추가

---
+++ 백준/Gold/11066. 파일 합치기(DP)/파일 합치기.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.split(" ").map((v) => +v));
+
+let [T] = input[0];
+let readIdx = 1;
+while (T--) {
+  const [N] = input[readIdx++];
+  const book = input[readIdx++];
+  const sum = [];
+  sum[0] = book[0];
+  for (let i = 1; i < N; i += 1) {
+    sum[i] = book[i] + sum[i - 1];
+  }
+  const dp = Array(N)
+    .fill()
+    .map(() => Array(N).fill(0));
+
+  for (let i = 1; i < N; i += 1) {
+    for (let j = 0; i + j < N; j += 1) {
+      dp[j][i + j] = Infinity;
+      for (let k = j; k < i + j; k += 1) {
+        dp[j][i + j] = Math.min(
+          dp[j][i + j],
+          dp[j][k] + dp[k + 1][j + i] + sum[i + j] - sum[j] + book[j]
+        );
+      }
+    }
+  }
+
+  console.log(dp[0][N - 1]);
+}
+
+// 행렬 곱셈 순서 문제(백준 11049)와 매우 유사한 문제
