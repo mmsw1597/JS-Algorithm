04a31690221defb6cf0df55276f1670e1ed33583
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-05-10 14:20:00 +0900

    [Gold III] Title: 앱, Time: 984 ms, Memory: 34284 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.split(" ").map((v) => +v));
+
+const [n, m] = input[0];
+const apps = input[1];
+const cost = input[2];
+
+const dp = Array(n + 1)
+  .fill()
+  .map(() => Array(100 * 100 + 1).fill(-Infinity));
+
+const DFS = (num, cur, mem) => {
+  if (num > n || dp[num][cur] >= mem) return;
+
+  dp[num][cur] = Math.max(mem, dp[num][cur]);
+  DFS(num + 1, cur + cost[num], mem + apps[num]);
+  DFS(num + 1, cur, mem);
+};
+
+DFS(0, 0, 0);
+let tmp = 0;
+
+while (dp[n][tmp] < m) {
+  tmp += 1;
+}
+
+console.log(tmp);
