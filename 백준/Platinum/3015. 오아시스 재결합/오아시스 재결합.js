3532849b15845c00861359c0c4340b240bb821cf
mmsw1597 <55936770+mmsw1597@users.noreply.github.com>
2023-05-12 13:46:09 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 464 ms, Memory: 105764 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.js
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.js
@@ -1,3 +1,10 @@
+class Node {
+  constructor(v, c) {
+    this.v = v;
+    this.c = c;
+  }
+}
+
 const fs = require("fs");
 const input = fs
   .readFileSync("/dev/stdin")
@@ -9,23 +16,22 @@ const input = fs
 const n = input.shift();
 const stack = [];
 let res = 0;
-let tmp = 0;
 
 for (let i = 0; i < n; i += 1) {
   let v = input[i];
   let c = 0;
 
-  while (stack.length && input[i] > stack[stack.length - 1][0]) {
+  while (stack.length && input[i] > stack[stack.length - 1].v) {
     stack.pop();
     res += 1;
   }
-  if (stack.length && input[i] === stack[stack.length - 1][0]) {
-    c = stack[stack.length - 1][1] + 1;
+  if (stack.length && input[i] === stack[stack.length - 1].v) {
+    c = stack[stack.length - 1].c + 1;
     res += c;
   }
   if (stack.length - c > 0) res += 1;
 
-  stack.push([input[i], c]);
+  stack.push(new Node(input[i], c));
 }
 
 console.log(res);
