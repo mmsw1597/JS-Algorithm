f1a90e33196b1655d5e7f3677419a76fa7970e27
Gun woo <55936770+mmsw1597@users.noreply.github.com>
2023-06-08 17:03:14 +0900

    [level 3] Title: 가장 먼 노드, Time: 29.34 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -0,0 +1,67 @@
+class Queue{
+    constructor(){
+        this.queue = [];
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    enqueue(value){
+        this.queue[this.rear++] = value;
+    }
+
+    dequeue(){
+        const ret = this.queue[this.front];
+        delete this.queue[this.front];
+        this.front++;
+        return ret;
+    }
+
+    isEmpty(){
+        return this.front === this.rear;
+    }
+
+    size(){
+        return this.rear - this.front;
+    }
+}
+
+
+function solution(n, edge) {
+    let answer = 0;
+    const graph = Array(n+1).fill().map(() => Array(0));
+    const dis = Array(n+1).fill(Infinity);
+    
+    for(let [s, v] of edge){
+        graph[v].push(s);
+        graph[s].push(v);
+    }
+    
+    const BFS = (level) => {
+        const queue = new Queue();
+        queue.enqueue(1);
+        dis[0] = 0;
+        dis[1] = 0;
+        const visited = Array(n+1).fill(false);
+        visited[1] = true;
+        let size = 0;
+        
+        while(!queue.isEmpty()){
+            size = queue.size();
+            for(let i = 0; i<size; i+=1){
+                const cur = queue.dequeue();
+                for(let next of graph[cur]){
+                    if(!visited[next]){
+                        visited[next] = true;
+                        dis[next] = level + 1;
+                        queue.enqueue(next);
+                    }
+                }   
+            }
+            level += 1;
+        }    
+        
+        return size;
+    }
+    
+    return BFS(0);
+}
