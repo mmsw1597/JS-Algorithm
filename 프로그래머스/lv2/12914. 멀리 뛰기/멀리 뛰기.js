b78523f3d0e9efab75c93b1e6f2a48f93b36e8e4
Gun woo <55936770+mmsw1597@users.noreply.github.com>
2023-06-13 15:07:14 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,13 +1,11 @@
 function solution(n) {
-    let answer = 0;
-    let dp = Array.from({length: n + 1}, ()=>0);
-    dp[0] = 1;
+    const dp = Array.from({length : n + 1}, () => 0);
     dp[1] = 1;
-    console.log(dp[n]);
-    for(let i = 2; i<=n; i++){
-        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    dp[0] = 1;
+    
+    for(let i = 2; i<=n; i+=1){
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
     }
     
-
     return dp[n];
 }
