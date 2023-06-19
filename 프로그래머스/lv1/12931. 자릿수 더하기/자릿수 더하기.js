22c2cbc174418882cc05e55f13840d89b629bb75
Gun woo <55936770+mmsw1597@users.noreply.github.com>
2023-06-19 16:15:23 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,5 +1,5 @@
 function solution(n){
-    let answer = String(n).split("").map((v) => +v).reduce((acc, v) => acc + v , 0);
+    let answer = String(n).split("").reduce((acc, v) => acc + +v, 0);
 
     return answer;
 }
