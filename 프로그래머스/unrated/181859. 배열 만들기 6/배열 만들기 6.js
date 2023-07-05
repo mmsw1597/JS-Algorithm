86007cee32616d206c5457fe5286f1312cdbe246
Gun woo <55936770+mmsw1597@users.noreply.github.com>
2023-07-06 02:20:47 +0900

    [unrated] Title: 배열 만들기 6, Time: 3.32 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,20 +1,4 @@
 function solution(arr) {
-    const answer = [];
-    
-    arr.forEach((v, i) => {
-        if(!answer.length){
-            answer.push(v);
-        }
-        else if(answer[answer.length - 1] === v){
-            answer.pop();   
-        }else{
-            answer.push(v);
-        }
-    })
-    
-    if(!answer.length){
-        answer.push(-1);
-    }
-    
+    var answer = [];
     return answer;
 }
