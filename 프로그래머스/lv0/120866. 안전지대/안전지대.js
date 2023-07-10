98bf37bbdec2a636a266aafe4833694dcc044124
Gun woo <55936770+mmsw1597@users.noreply.github.com>
2023-07-10 19:28:47 +0900

    [level 0] Title: 안전지대, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,16 +1,15 @@
 function solution(board) {
     let answer = 0;
-    let d = [[-1, 0], [1, 0], [0, -1], [0, 1], [1,1], [1,-1], [-1,1], [-1,-1]];
-    let n = board.length;
+    const N = board.length;
+    const d = [[1, 1], [1, -1], [-1, 1], [-1, -1], [0, 1], [0, -1], [1, 0], [-1, 0]];
     
-    for(let i = 0; i<n; i++){
-        for(let j = 0; j<n; j++){
-            if(board[i][j] === 1){
+    for(let y = 0; y < N; y++){
+        for(let x = 0; x < N; x++){
+            if(board[y][x] === 1){
                 for(let [dy, dx] of d){
-                    let ny = i + dy;
-                    let nx = j + dx;
-
-                    if(nx >= 0 && nx < n && ny >= 0 && ny < n && board[ny][nx] === 0){
+                    let ny = y + dy;
+                    let nx = x + dx;
+                    if(ny >= 0 && ny < N && nx >= 0 && nx < N && !board[ny][nx]){
                         board[ny][nx] = 2;
                     }
                 }
@@ -18,11 +17,13 @@ function solution(board) {
         }
     }
     
-    for(let i = 0; i<n; i++){
-        for(let j = 0; j<n; j++){
-            if(!board[i][j]) answer++;
+   for(let y = 0; y < N; y++){
+        for(let x = 0; x < N; x++){
+            if(!board[y][x]){
+                answer++;
+            }
         }
-    }
+   }
     
     return answer;
 }
